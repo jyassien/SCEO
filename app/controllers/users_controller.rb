c9401f1310a1efc5e069835a33b010969d6819c4
jyassien <jihadumuhammed@gmail.com>
2024-12-04 09:52:06 -0700

    updates nav buttons for sign in and out

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -60,12 +60,16 @@ class UsersController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_user
-      if params[:id] == "sign_out"
-        redirect_to root_path
-      else
-        @user = User.find(params[:id])
-      end
+      @user = User.find(params[:id])
     end
+
+    # def set_user
+    #   if params[:id] == "sign_out"
+    #     redirect_to root_path
+    #   else
+    #     @user = User.find(params[:id])
+    #   end
+    # end
 
     # Only allow a list of trusted parameters through.
     def user_params
